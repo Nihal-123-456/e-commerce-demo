6f8a1ba9ad0952a0d5e5d5d893b76d2bf8c9df40
unknown <knasn1526@gmail.com>
2026-07-15 18:27:16 +0600

    fixing card size

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -22,7 +22,7 @@ const UserDashboard = async ({groceryList}:{groceryList:IGrocery[]}) => {
             {groceryList.length} items available
           </p>
         </div>
-        <div className='grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        <div className='grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
           {groceryList.map((item:any, index) => (
             <GroceryItemCard item={item} key={index}/>
           ))}
